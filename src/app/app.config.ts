f4d5d1fd81c257625f175d591c2e724bc4df6b40
Kozma Kristof <bechryko@gmail.com>
2024-12-17 19:17:40 +0100

    OJ-2 Setup transloco

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,30 @@
-import { ApplicationConfig, provideExperimentalCheckNoChangesForDebug, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideExperimentalCheckNoChangesForDebug,
+  provideExperimentalZonelessChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideTransloco } from '@jsverse/transloco';
 import { routes } from './app.routes';
+import { TranslocoHttpLoader } from './transloco-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     // eslint-disable-next-line no-magic-numbers
     provideExperimentalCheckNoChangesForDebug({ interval: 10 }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en'],
+        defaultLang: 'en',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode()
+      },
+      loader: TranslocoHttpLoader
+    })
   ]
 };
