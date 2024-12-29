b32ac73bd47aebbc7cbd8365aecda07eb333c4be
Kozma Kristof <bechryko@gmail.com>
2024-12-29 02:01:33 +0100

    OJ-8 Create basic animation model

---
+++ src/app/game/core/entities/animations/animation.ts
@@ -0,0 +1,4 @@
+import { Drawable } from '../../../drawing';
+import { OnTick } from '../entity-hooks';
+
+export type Animation = Drawable & OnTick;
---
+++ src/app/game/core/entities/entity-hooks.d.ts
@@ -0,0 +1,3 @@
+export interface OnTick {
+  onTick(deltaTime: number): void;
+}
