b17037e32c60add6c8fe51dea0abb4af2ad38cb3
Kozma Kristof <bechryko@gmail.com>
2025-01-28 17:50:48 +0100

    OJ-11 Scale moving attribute's speed with higher score

--- src/app/game/core/entities/platform-attributes/moving-attribute.ts
+++ src/app/game/core/entities/platform-attributes/moving-attribute.ts
@@ -1,12 +1,18 @@
 import { GameRandomUtils } from '@omegajump-core/control';
+import { CameraUtils } from '@omegajump-core/physics';
 import { PlatformAttribute } from './platform-attribute';
 
 export class MovingAttribute extends PlatformAttribute {
   private static readonly MIN_SPEED = 3;
+  private static readonly BONUS_MIN_SPEED_PER_1000_POINTS = 2;
   private static readonly MAX_SPEED = 8;
+  private static readonly BONUS_MAX_SPEED_PER_1000_POINTS = 3;
 
   private readonly direction = GameRandomUtils.choose([-1, 1]);
-  private readonly speed = GameRandomUtils.between(MovingAttribute.MIN_SPEED, MovingAttribute.MAX_SPEED);
+  private readonly speed = GameRandomUtils.between(
+    MovingAttribute.MIN_SPEED + (CameraUtils.y / 1000) * MovingAttribute.BONUS_MIN_SPEED_PER_1000_POINTS,
+    MovingAttribute.MAX_SPEED + (CameraUtils.y / 1000) * MovingAttribute.BONUS_MAX_SPEED_PER_1000_POINTS
+  );
 
   public override onTick(deltaTime: number): void {
     this.hostPlatform.position.x += this.direction * this.speed * deltaTime;
